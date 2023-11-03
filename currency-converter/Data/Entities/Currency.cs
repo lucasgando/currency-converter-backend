a687a2ef0d6e99d93ed0cae329088bb40cf3f80f
Lucas Gandossini <lucas.gandossini@outlook.com>
2023-11-03 17:59:35 -0300

    Added schema annotations

--- currency-converter/Data/Entities/Currency.cs
+++ currency-converter/Data/Entities/Currency.cs
@@ -1,7 +1,12 @@
-﻿namespace currency_converter.Data.Entities
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace currency_converter.Data.Entities
 {
     public class Currency
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public string Name { get; set; }
         public string Symbol { get; set; }
