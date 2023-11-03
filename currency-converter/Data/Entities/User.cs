aa051cfb6ca6dfadce5d245e5af69f476c079908
Lucas Gandossini <lucas.gandossini@outlook.com>
2023-11-03 17:59:20 -0300

    Created user entity with schema annotations

---
+++ currency-converter/Data/Entities/User.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace currency_converter.Data.Entities
+{
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public int SubscriptionId { get; set; }
+        [ForeignKey(nameof(SubscriptionId))]
+        public Subscription Subscription { get; set; }
+
+    }
+}
