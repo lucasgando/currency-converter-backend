d336b5deffd23708f77b8d26e510b0cd13aa95a9
Lucas Gandossini <lucas.gandossini@outlook.com>
2023-11-03 17:59:59 -0300

    Created subscription entity with schema annotations

---
+++ currency-converter/Data/Entities/Subscription.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace currency_converter.Data.Entities
+{
+    public class Subscription
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool HasConverterLimit { get; set; }
+        public int ConverterLimit { get; set; }
+        public int UsdPrice { get; set; }
+    }
+}
