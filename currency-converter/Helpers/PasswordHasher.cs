8101664cd8dc5018e24b3b9b81264840cff2f2ac
Lucas Gandossini <lucas.gandossini@outlook.com>
2023-11-03 17:53:33 -0300

    Created password hasher static class

---
+++ currency-converter/Helpers/PasswordHasher.cs
@@ -0,0 +1,17 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace currency_converter.Helpers
+{
+    public static class PasswordHasher
+    {
+        public static string GetPasswordHash(string password)
+        {
+            StringBuilder hashedPassword = new StringBuilder();
+            byte[] byteHash = SHA256.HashData(Encoding.UTF8.GetBytes(password));
+            foreach (byte theByte in byteHash)
+                hashedPassword.Append(theByte.ToString("x2"));
+            return hashedPassword.ToString();
+        }
+    }
+}
