39723c1313dafeec7754d38489b55f111b51b215
Lucas Gandossini <lucas.gandossini@outlook.com>
2023-11-03 17:49:30 -0300

    Created currency entity

---
+++ currency-converter/Data/Entities/Currency.cs
@@ -0,0 +1,10 @@
+﻿namespace currency_converter.Data.Entities
+{
+    public class Currency
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Symbol { get; set; }
+        public float ConversionIndex {  get; set; }
+    }
+}
